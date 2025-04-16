0bdc1e70e0dc863be2f5f0461014c843be7b9e3e
Artem_Teplygin <teplygin07@mail.ru>
2025-04-16 22:47:08 +0300

    completed HomeWork №8

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -9,7 +9,7 @@ function Junior() {
         <div id={'hw5-page-junior'}>
             <HW6 />
             <HW7 />
-            {/*<HW8 />*/}
+            <HW8 />
             {/*<HW9 />*/}
         </div>
     )
